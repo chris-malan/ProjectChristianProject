80a4be468108659b4024ce860e8b0db79e73b0cc
Christian Malan <malanc@sheridancollege.ca>
2026-01-28 22:00:52 -0500

    adding comment for testing in feature

---
+++ ProjectChristianClone1/src/main/java/ca/sheridancollege/malanc/projectchristianclone1/ProjectChristianClone1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ca.sheridancollege.malanc.projectchristianclone1;
+
+/**
+ *
+ * @author Chistan M
+ */
+
+/*
+this is the comment for feature
+*/
+
+/*
+adding comment for changes in feature branch
+*/
+public class ProjectChristianClone1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
